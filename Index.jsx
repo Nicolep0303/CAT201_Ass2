b57c6b57a388ae5de540c9aa46d82e0bb7bfbc36
Nicolep0303 <121543620+Nicolep0303@users.noreply.github.com>
2024-01-07 20:25:11 +0800

    Add files via upload

---
+++ Index.jsx
@@ -0,0 +1,343 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="about-us">
+          <div className="overlap-group">
+            <p className="text-wrapper">IT That Works For You</p>
+            <div className="text-wrapper-2">Our Company</div>
+            <div className="video-wrapper">
+                <iframe
+                  width="913"
+                  height="514"
+                  src="https://www.youtube.com/embed/cR1FyHv_rJE?si=spcWeExc42HImK8a"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
+              </div>
+          </div>
+        </div>
+        <div className="frame">
+          <div className="services">
+            <div className="frame-2">
+              <div className="text-wrapper-3">Our Services</div>
+              <div className="frame-3">
+                <div className="service">
+                  <div className="text-wrapper-4">Managed Services</div>
+                  <p className="we-ll-manage-all-of">
+                    We’ll manage all of your IT
+                    <br />
+                    services for a flat-rate fee
+                  </p>
+                  <img
+                    className="business-concept"
+                    alt="Business concept"
+                    src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/business-concept-with-wooden-blocks-with-icons-green-plant-1.png"
+                  />
+                </div>
+                <div className="service-2">
+                  <div className="data-backup-recovery">Data Backup &amp; Recovery</div>
+                  <p className="prevent-the">
+                    Prevent the unexpected
+                    <br />
+                    from rulning your company
+                  </p>
+                  <img
+                    className="cloud-drawer"
+                    alt="Cloud drawer"
+                    src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/cloud-drawer-2.png"
+                  />
+                </div>
+                <div className="service-3">
+                  <div className="text-wrapper-5">Cloud Computing</div>
+                  <p className="our-solutions-are">
+                    Our solutions are fully scalable
+                    <br />
+                    and available for a flat-rate fee
+                  </p>
+                  <img
+                    className="img"
+                    alt="Rectangle"
+                    src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/rectangle-3@2x.png"
+                  />
+                </div>
+                <div className="service-4">
+                  <div className="text-wrapper-6">Virtualization</div>
+                  <p className="stop-buying-servers">
+                    Stop buying servers and
+                    <br />
+                    optimize the pnes you have
+                  </p>
+                  <img
+                    className="img"
+                    alt="Rectangle"
+                    src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/rectangle-3-1@2x.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="feedback">
+            <div className="overlap-group-2">
+              <img
+                className="pexels-photo-by"
+                alt="Pexels photo by"
+                src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/pexels-photo-by-james-wheeler.png"
+              />
+              <p className="see-what-other">
+                See What Other Business Owners
+                <br />
+                Are Saying About Us
+              </p>
+              <div className="group">
+                <img
+                  className="img-2"
+                  alt="Left arrow"
+                  src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/left-arrow@2x.png"
+                />
+              </div>
+              <div className="right-arrow-wrapper">
+                <img
+                  className="img-2"
+                  alt="Right arrow"
+                  src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598ce4a00be5f53b734f593/img/right-arrow@2x.png"
+                />
+              </div>
+              <div className="frame-wrapper">
+                <div className="frame-4">
+                  <div className="comment">
+                    <div className="text-wrapper-7">Bright Lighting Design</div>
+                    <div className="text-wrapper-8">Charlene Wong</div>
+                    <div className="founder-president">Founder &amp; President</div>
+                    <p className="p">
+                      &#34;IT assistance and support is paramount to the daily success of my business. You have to rely
+                      upon someone who is committed to your business as much as you are. The Team at TechVista has met
+                      our need in this respect. Our relationship over the past 10 years has helped my company grow and
+                      continue to service our demanding clients.&#34;
+                    </p>
+                    <img
+                      className="ellipse"
+                      alt="Ellipse"
+                      src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/ellipse-4@2x.png"
+                    />
+                  </div>
+                  <div className="comment-2">
+                    <div className="text-wrapper-9">FutureWorks Finance</div>
+                    <div className="text-wrapper-10">Daud Hassan</div>
+                    <div className="text-wrapper-11">President</div>
+                    <p className="p">
+                      &#34;With TechVista we have professionals who do this every day, 365 days a year, 7 days a week,
+                      24 hours a day that are constantly keeping us abreast of potential security breaches and potential
+                      hazards to things that would compromise the sensitive financial data we have.”
+                    </p>
+                    <img
+                      className="ellipse"
+                      alt="Ellipse"
+                      src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/ellipse-4-1@2x.png"
+                    />
+                  </div>
+                  <div className="comment-3">
+                    <div className="text-wrapper-12">Rock Vermon PLLC</div>
+                    <div className="text-wrapper-13">Arthur Vermon</div>
+                    <div className="lawyer-partner">Lawyer &amp; Partner</div>
+                    <p className="p">
+                      &#34;Our law firm takes pride in being on the front end of technology in the legal market.
+                      We&#39;ve stayed ahead of the curve, without having to hire in-house IT, thanks to the talented
+                      and trustworthy TechVista team. I originally hired TechVista because of the high integrity of its
+                      founder, Norita. It&#39;s clear that his approach has rubbed off on his team. I trust this bunch
+                      and they do an amazing job.&#34;
+                    </p>
+                    <img
+                      className="ellipse"
+                      alt="Ellipse"
+                      src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/ellipse-4-2@2x.png"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="our-team">
+          <div className="overlap-group-2">
+          <div className="video-wrapper1">
+                <iframe
+                  width="1920"
+                  height="1080"
+                  src="https://www.youtube.com/embed/NvqB-JwhWh0?si=aFJfjS-vGfKJuzCe&autoplay=1&mute=1"
+                  frameborder="0"
+                  allowfullscreen
+                  controls={false}
+                ></iframe>
+            </div>
+            <div className="text-wrapper-14">Meet Our Team</div>
+            <div className="group-2">
+              <div className="team">
+                <div className="NORITA-BINTI-MUIN">
+                  NORITA BINTI
+                  <br />
+                  MUIN
+                </div>
+                <img
+                  className="image"
+                  alt="Image"
+                  src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/image-3.png"
+                />
+              </div>
+              <p className="norita-the-visionary">
+                Norita, the visionary founder of TechVista, laid the foundation for the company in 2005. With a passion
+                for technology and a keen entrepreneurial spirit, Norita played a pivotal role in shaping the
+                company&#39;s values and mission. As the driving force behind TechVista&#39;s inception, Norita
+                continues to inspire the team with a commitment to excellence and a forward-thinking approach to IT
+                solutions.
+              </p>
+              <div className="team-2">
+                <div className="ASILAH-ZARIFAH-BINTI">
+                  ASILAH ZARIFAH
+                  <br />
+                  BINTI ROSLI
+                </div>
+                <img
+                  className="image-2"
+                  alt="Image"
+                  src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/image-4.png"
+                />
+              </div>
+              <p className="as-the-chief">
+                As the Chief Executive Officer of TechVista, Zarie brings strategic leadership and a wealth of
+                experience to the helm. With a focus on guiding the company towards success, Zarie oversees the
+                day-to-day operations and ensures that TechVista remains at the forefront of the ever-evolving
+                technology landscape. Under Zarie&#39;s leadership, the company continues to thrive, delivering
+                top-notch IT services and solutions to businesses in Penang.
+              </p>
+              <div className="team-3">
+                <div className="text-wrapper-15">NG PEI WEI</div>
+                <img
+                  className="image-3"
+                  alt="Image"
+                  src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/image-1@2x.png"
+                />
+              </div>
+              <p className="pei-wei-the-chief">
+                Pei Wei, the Chief Technology Officer at TechVista, is the driving force behind the company&#39;s
+                technological innovations. With a deep understanding of emerging trends and a passion for cutting-edge
+                solutions, Pei Wei leads the technology team in developing and implementing state-of-the-art IT
+                solutions. Through Pei Wei&#39;s expertise, TechVista remains a trailblazer in providing businesses with
+                advanced and reliable technology services.
+              </p>
+              <div className="team-4">
+                <div className="text-wrapper-16">NICOLE PERERA</div>
+                <img
+                  className="image-4"
+                  alt="Image"
+                  src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a8350ea1c0897b96a630a/img/image-2.png"
+                />
+              </div>
+              <p className="nicole-as-the-chief">
+                Nicole, as the Chief Financial Officer of TechVista, is entrusted with ensuring the financial health and
+                stability of the company. With a keen eye for detail and a strategic financial mindset, Nicole manages
+                the financial aspects of the business, contributing to its growth and sustainability. Nicole&#39;s role
+                is crucial in maintaining transparency and financial integrity, allowing TechVista to deliver
+                consistent, high-quality IT services to its clients.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="bottom-header">
+          <p className="we-will-further">
+            We will further support you on reaching
+            <br />
+            YOUR stated TARGETS within your
+            <br />
+            journey to success
+          </p>
+          <img
+            className="frame-5"
+            alt="Frame"
+            src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598f7cfd85112d634558be7/img/frame-3.svg"
+          />
+          <div className="group-3">
+            <div className="text-wrapper-17">TechVista</div>
+            <img
+              className="TV"
+              alt="Tv"
+              src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a4abf00be5f53b734f7e2/img/tv-1@2x.png"
+            />
+          </div>
+          <div className="text-wrapper-18">Contact Info</div>
+          <p className="text-wrapper-19">
+            No.30 Jalan Cassia Selatan 1/3, Taman Perindustrian Batu Kawan, Bandar Cassia, 14110, Pulau Pinang,
+            Malaysia.
+          </p>
+          <div className="place-marker-wrapper">
+            <img
+              className="place-marker"
+              alt="Place marker"
+              src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598f7cfd85112d634558be7/img/place-marker@2x.png"
+            />
+          </div>
+          <div className="text-wrapper-20">+604 331 3030</div>
+          <div className="phone-wrapper">
+            <img
+              className="phone"
+              alt="Phone"
+              src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598f7cfd85112d634558be7/img/phone@2x.png"
+            />
+          </div>
+          <div className="text-wrapper-21">enquiry@techvista.com</div>
+          <div className="email-wrapper">
+            <img
+              className="email"
+              alt="Email"
+              src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598f7cfd85112d634558be7/img/email@2x.png"
+            />
+          </div>
+          <div className="text-wrapper-22">Copyright © 2024 TechVista_CAT201_GROUP_30</div>
+        </div>
+        <div className="main-text">
+          <div className="overlap">
+            <div className="rectangle-2" />
+            <div className="text">
+              MANAGED &amp; RELIABLE
+              <br />
+              IT SUPPORT
+            </div>
+            <p className="text-wrapper-23">
+              Are You Searching For Reliable IT Support in Penang Area? Tired of IT Guys with Poor Response Time,
+              Endless Invoices, and Uncertainty About Compliance Regulations?
+            </p>
+            <div className="explore">
+              <div className="text-wrapper-24">Explore More</div>
+            </div>
+          </div>
+        </div>
+        <div className="top-header">
+          <div className="navbar">
+            <div className="text-wrapper-25">Home</div>
+            <div className="text-wrapper-26">About Us</div>
+            <div className="text-wrapper-26">Services</div>
+            <div className="text-wrapper-26">Our Team</div>
+            <div className="text-wrapper-26">Contact Us</div>
+          </div>
+          <img
+            className="frame-6"
+            alt="Frame"
+            src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/6598d602f8c0a7ce7bd371d1/img/frame-3.svg"
+          />
+          <div className="group-4">
+            <div className="text-wrapper-17">TechVista</div>
+            <img
+              className="TV"
+              alt="Tv"
+              src="https://cdn.animaapp.com/projects/65988b9900be5f53b734f52e/releases/659a4abf00be5f53b734f7e2/img/tv-1@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
